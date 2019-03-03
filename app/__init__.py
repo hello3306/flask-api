72652621fa4dfcc6649c0bd7db5b25ab36a41642
longjin <1176837406@qq.com>
2019-03-03 21:20:34 +0800

    '7.8'

--- app/__init__.py
+++ app/__init__.py
@@ -2,3 +2,32 @@
  Hello 
 
 """
+from app.app import Flask
+
+
+def register_blueprints(app):
+    from app.api.v1 import create_blueprint_v1
+    app.register_blueprint(create_blueprint_v1(), url_prefix='/v1')
+
+
+def register_plugin(app):
+    from app.models.base import db
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
+
+
+def create_app():
+    app = Flask(__name__)
+
+    # 加载配置文件
+    app.config.from_object('app.config.setting')
+    app.config.from_object('app.config.secure')
+
+    # 蓝图注册
+    register_blueprints(app)
+
+    # 数据库模型的注册
+    register_plugin(app)
+
+    return app
--- app/app.py
+++ app/app.py
@@ -5,39 +5,20 @@
 from flask import Flask as _Flask
 from flask.json import JSONEncoder as _JSONEncoder
 
+from app.libs.error_code import ServerError
+from datetime import date
+
 
 class JSONEncoder(_JSONEncoder):
     def default(self, o):
-        return dict(o)
+        if hasattr(o, 'keys') and hasattr(o, '__getitem__'):
+            return dict(o)
+        if isinstance(o, date):
+            return o.strftime('%Y-%m-%d')
+        raise ServerError()
 
 
 class Flask(_Flask):
     json_encoder = JSONEncoder
 
 
-def register_blueprints(app):
-    from app.api.v1 import create_blueprint_v1
-    app.register_blueprint(create_blueprint_v1(), url_prefix='/v1')
-
-
-def register_plugin(app):
-    from app.models.base import db
-    db.init_app(app)
-    with app.app_context():
-        db.create_all()
-
-
-def create_app():
-    app = Flask(__name__)
-
-    # 加载配置文件
-    app.config.from_object('app.config.setting')
-    app.config.from_object('app.config.secure')
-
-    # 蓝图注册
-    register_blueprints(app)
-
-    # 数据库模型的注册
-    register_plugin(app)
-
-    return app
